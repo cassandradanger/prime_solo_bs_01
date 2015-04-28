852cd9cc99efd4aa236a63dae5fac7a80df133ef
Casie Lynch <cds.lynch@gmail.com>
2015-04-28 16:19:18 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-var apikey = ''; // Put your API key here
+var apikey = 'ba7280b6a052562af59a3776f9607c43984e6b6a';
 
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
+	for(var i = 0; i < 9; i++){
+		var name = results[i].name;
+		var image = results[i].image.icon_url;
+		var deck = results[i].deck;
+		if(i % 3 == 0){
+			var count = i;
+			$(".container").append("<div class = 'row row" + count + "'></div>");
+		};
+		$(".row" + count).append("<div class = 'main col-md-4 well'><p class = 'lead'>" + name + "</p><div class = 'hidden-sm hidden-xs'><img src = '" + image + "'></div><button class ='btnRemove btn-sm btn-success'>remove</button><p>" + deck + "</p></div>");
+		$(".row" + count).hide().fadeIn();
+
+	}
     console.log(results);
 }
 
 $(document).ready(function() {
+	count = 0
+	search('batman');
+	$(".container").on("click", ".btnRemove", function(){ 
+		count++;
+		$(this).parent().fadeOut(".main");
+	});
 
-	// Start the search here!
-	
 });
+
+
 
 // HELPER FUNCTION
 // Executes a search using 'query' and runs searchCallback on the results of a success.
